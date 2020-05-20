b35717c475a7b410fdb5c4163c20e075e6fbea49
Zachary Mansell <zacharymansell@Zacharys-MacBook-Pro.local>
2020-05-20 13:38:29 -0500

    breaks up a camel case string with spaces

---
+++ breakCamelCase.js
@@ -0,0 +1,27 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+// Example
+// solution("camelCasing")  ==  "camel Casing"
+
+function solution(str) {
+  return str.split(/(?=[A-Z])/).join(" ");
+}
+// /  / signifies the start of the regular expression
+// (?=) is "positive lookahead", matches a group after the main expression without including it in result
+//removing the positive lookahead returns [camel, C, asing]
+//
+// [] "character set", matches any character in the set
+// A-Z matches characters in the range from A to Z, case sensitive
+
+
+function assertEquals(actual, expected){
+  if (actual === expected){
+    console.log(`passed`);
+  } else {
+    console.log(`failed Expected "${expected}" but got "${actual}"`);
+  }
+}
+let string = "camelCasing";
+let inputActual = solution(string);
+let inputExpected = "camel Casing";
+assertEquals(inputActual, inputExpected);
+console.log(string.split(/(?=[A-Z])/));
