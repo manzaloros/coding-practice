b32d98be4ad7d673408620465f00d1558c67631b
manzaloros <zacharymansellcello@gmail.com>
2020-10-01 08:54:51 -0700

    Implement numberOfRecentCalls

--- leetcode-problems/firstMissingPositive.js
+++ leetcode-problems/firstMissingPositive.js
@@ -10,7 +10,6 @@ Needs to run in O(n) time and constant space (this solution is not constant spac
 // First missing number can be in range from 1 to nums.length
 const firstMissingPositive = (nums) => {
   const array = new Array(nums.length).fill(-1);
-  let missing = 0;
   for (let i = 0; i < nums.length; i += 1) {
     if (nums[i] > 0) {
       // Will create empty spaces in array if adding to an index greater than the current length
@@ -21,8 +20,8 @@ const firstMissingPositive = (nums) => {
     // Since the array was originally filled with -1s, it will return 1 if the first element is a -1
     if (array[i] === -1) {
       return i + 1;
-      break;
     }
   }
+  // Return
   return array.length + 1;
 }
