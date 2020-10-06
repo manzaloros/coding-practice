366e484df87996851b1d9323bdc15d96b1f7be1e
manzaloros <zacharymansellcello@gmail.com>
2020-10-06 08:56:05 -0700

    Implement daily and easy problems

--- leetcode-problems/bitwiseComplement.js
+++ leetcode-problems/bitwiseComplement.js
@@ -21,7 +21,6 @@ Explanation: 10 is "1010" in binary, with complement "0101" in binary, which is 
 
 const bitwiseComplement = (n) => {
   let binary = n.toString(2).split('');
-  let complement = 0;
   for (let i = 0; i < binary.length; i += 1) {
     binary[i] = binary[i] === '0' ? '1' : '0'
   }
@@ -29,3 +28,28 @@ const bitwiseComplement = (n) => {
 }
 
 console.log(bitwiseComplement(5))
+
+// Adam Tiller Solution:
+/*
+
+var bitwiseComplement = function(N) {
+  let pending = 0;
+  let result = 0;
+
+  for (let i = 0; i < 32; i++) {
+      console.log(`1 << ${i}: `, (1 << i).toString(2));
+      console.log(`${1 << i} & ${(5).toString(2)}: `, ((1 << i) & N).toString(2));
+    if ((1 << i) & N) {
+      result += pending;
+      pending = 0;
+    } else {
+      pending += 1 << i;
+      console.log(`1 << ${i}: ${1 << i}`)
+      console.log(`pending += ${1 << i}`)
+    }
+  }
+
+  return N ? result : 1;
+};
+
+*/
