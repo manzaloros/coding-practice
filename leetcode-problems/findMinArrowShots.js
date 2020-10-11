5391d7a873bda41f55a42e6f2d45ae4840fc2418
manzaloros <zacharymansellcello@gmail.com>
2020-10-10 17:34:24 -0700

    Add findMinArrowShots correct sorted

--- leetcode-problems/findMinArrowShots.js
+++ leetcode-problems/findMinArrowShots.js
@@ -13,53 +13,19 @@ Explanation: One way is to shoot one arrow for example at x = 6 (bursting the ba
 
 const findMinArrowShots = (points) => {
   if (points.length === 0) return 0;
-  // Create object to track balloon overlap
-  const balloonOverlaps = {};
-  // for each pair of balloons
-  for (let i = 0; i < points.length; i += 1) {
-    // for each balloon
-    const balloon1 = points[i];
-    balloonOverlaps[i] = [];
-    let ownBalloon = true;
-    for (let j = 0; j < points.length; j += 1) {
-      if (j === i) continue;
-      const balloon2 = points[j];
-      // if the balloon1[0] <= balloon2[1] && balloon1[1] >= balloon2[0]
-      if (balloon1[0] <= balloon2[1] && balloon1[1] >= balloon2[0]) {
-        // add balloon1 and balloon2 to array
-        balloonOverlaps[i].push([balloon1, balloon2]);
-        ownBalloon = false;
-      } else {
-        // otherwise, balloon needs it's own arrow
-        // add balloon to array
-        // Check for other combination in balloonOverlaps
-        let overlapFound = false;
-        for (let k = 0; k < balloonOverlaps[i].length; k += 1) {
-          const compareBalloon = balloonOverlaps[i][k];
-          if (balloon2[0] <= compareBalloon[1] && balloon2[1] >= compareBalloon[0]) {
-            balloonOverlaps[i][k] = [compareBalloon, balloon2];
-            overlapFound = true;
-          }
-        }
-        if (overlapFound) {
-          continue;
-        }
-        balloonOverlaps[i].push(balloon2);
-      }
 
-    }
-    // If no match has been found for balloon1,
-    // There is no overlap, add it to object
-    if (ownBalloon) {
-      balloonOverlaps[i].push(balloon1);
-    }
+  points.sort((a, b) => a[1] - b[1]);
+
+  let end = points[0][1];
+
+  let result = 1;
+  for (let i = 0; i < points.length; i += 1) {
+    const point = points[i];
+    if (point[0] <= end) continue;
+    result += 1;
+    end = point[1];
   }
-  const numberOfArrows = [];
-  // Iterate through object keys to find smallest length array
-  for (const combinations in balloonOverlaps) {
-    numberOfArrows.push(balloonOverlaps[combinations].length);
-  }
-  return Math.min(...numberOfArrows);
+  return result
 }
 
 // console.log(findMinArrowShots([[10, 16], [2, 8], [1, 6], [7, 12]])); // 2
