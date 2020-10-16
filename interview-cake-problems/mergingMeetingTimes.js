1e9726bdee99b9c467e38384a1653764f58bd877
manzaloros <zacharymansellcello@gmail.com>
2020-10-16 07:53:15 -0700

    Add mergeSort1

---
+++ interview-cake-problems/mergingMeetingTimes.js
@@ -0,0 +1,101 @@
+/* Your company built an in-house calendar tool called HiCal. You want to add a feature to see the times in a day when everyone is available.
+
+To do this, you’ll need to know when any team is having a meeting. In HiCal, a meeting is stored as objects ↴ with integer properties startTime and endTime. These integers represent the number of 30-minute blocks past 9:00am.
+
+For example:
+
+  { startTime: 2, endTime: 3 }  // meeting from 10:00 – 10:30 am
+{ startTime: 6, endTime: 9 }  // meeting from 12:00 – 1:30 pm
+
+JavaScript
+Write a function mergeRanges() that takes an array of multiple meeting time ranges and returns an array of condensed ranges.
+
+For example, given:
+
+  [
+  { startTime: 0,  endTime: 1 },
+  { startTime: 3,  endTime: 5 },
+  { startTime: 4,  endTime: 8 },
+  { startTime: 10, endTime: 12 },
+  { startTime: 9,  endTime: 10 },
+]
+
+JavaScript
+your function would return:
+
+  [
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 8 },
+  { startTime: 9, endTime: 12 },
+]
+
+JavaScript
+Do not assume the meetings are in order. The meeting times are coming from multiple teams.
+
+Write a solution that's efficient even when we can't put a nice upper bound on the numbers representing our time ranges. Here we've simplified our times down to the number of 30-minute slots past 9:00 am. But we want the function to work even for very large numbers, like Unix timestamps. In any case, the spirit of the challenge is to merge meetings where startTime and endTime don't have an upper bound.
+
+*/
+
+/*
+Time: O(n log n), sort is log n and iterating once is O(n)
+Space: O(n) since worst case we will have to populate our array with all new meeting ranges
+*/
+const mergeRanges = (meetings) => {
+  const sortedMeetings = meetings.sort((a, b) => a.startTime - b.startTime); // Sort by start times
+
+  let { startTime, endTime } = sortedMeetings[0]
+
+  const merged = [sortedMeetings[0]];
+  for (let i = 1; i < sortedMeetings.length; i += 1) {
+
+    const [currentStart, currentEnd] = [sortedMeetings[i].startTime, sortedMeetings[i].endTime]
+    if (currentStart <= endTime) {
+      endTime = Math.max(currentEnd, endTime) // If the previous end time is greater than the current, make it the new end time
+      merged[merged.length - 1].endTime = endTime; // Make the most recent interval end time the new (or old) end time
+    } else {
+      const range = { startTime: currentStart, endTime: currentEnd };
+      endTime = currentEnd;
+      merged.push(range);
+    }
+  }
+
+  return merged;
+}
+
+/* let desc = 'meetings overlap';
+let actual = mergeRanges([{ startTime: 1, endTime: 3 }, { startTime: 2, endTime: 4 }]);
+let expected = [{ startTime: 1, endTime: 4 }];
+console.log(actual, expected, desc);
+ */
+
+/* desc = 'meeting contains other meeting';
+actual = mergeRanges([{ startTime: 1, endTime: 8 }, { startTime: 2, endTime: 5 }]);
+expected = [{ startTime: 1, endTime: 8 }];
+assertArrayEquals(actual, expected, desc); */
+/*
+desc = 'meetings stay separate';
+actual = mergeRanges([{ startTime: 1, endTime: 3 }, { startTime: 4, endTime: 8 }]);
+expected = [{ startTime: 1, endTime: 3 }, { startTime: 4, endTime: 8 }];
+assertArrayEquals(actual, expected, desc); */
+
+/* desc = 'meetings not sorted';
+actual = mergeRanges([
+  { startTime: 5, endTime: 8 },
+  { startTime: 1, endTime: 4 },
+  { startTime: 6, endTime: 8 },
+]);
+expected = [{ startTime: 1, endTime: 4 }, { startTime: 5, endTime: 8 }];
+assertArrayEquals(actual, expected, desc); */
+
+desc = 'oneLongMeetingContainsSmallerMeetings';
+actual = mergeRanges([
+  { startTime: 1, endTime: 10 },
+  { startTime: 2, endTime: 5 },
+  { startTime: 6, endTime: 8 },
+  { startTime: 9, endTime: 10 },
+  { startTime: 10, endTime: 12 }
+]);
+expected = [
+  { startTime: 1, endTime: 12 }
+];
+assertArrayEquals(actual, expected, desc);
---
+++ merge-sort/mergeSort1.js
@@ -0,0 +1,45 @@
+const mergeSort = (nums) => {
+  if (nums.length === 1) return nums;
+
+  const halfway = Math.floor(nums.length / 2);
+  const half1 = nums.slice(0, halfway);
+  const half2 = nums.slice(halfway, nums.length);
+
+  return merge(mergeSort(half1), mergeSort(half2));
+}
+
+
+const merge = (left, right) => {
+  let [i, j] = [0, 0];
+
+  const sorted = [];
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      sorted.push(left[i++]);
+    } else {
+      sorted.push(right[j++]);
+    }
+  }
+
+  const remaining = i === left.length ? right.slice(j) : left.slice(i);
+  return sorted.concat(remaining);
+}
+
+const assertArraysEqual = (actual, expected) => {
+  if (!Array.isArray(actual) || !Array.isArray(expected)) return console.log(`invalid input`);
+  if (actual.length !== expected.length) return console.log(`Non-matching lengths`);
+  let areEqual = true;
+
+  actual.forEach((n, i) => {
+    if (n !== expected[i]) {
+      areEqual = false;
+    }
+  })
+
+  return console.log(`${areEqual}. Expected [${expected}]${areEqual ? ' and' : ', but'} got [${actual}]`);
+}
+
+
+const array = [5, 4, 3, 2, 1];
+assertArraysEqual(mergeSort(array), array.sort((a, b) => a - b));
