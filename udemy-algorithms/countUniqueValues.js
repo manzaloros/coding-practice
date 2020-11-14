448d369c1e06f792fcff99759d057c15c49ddc34
manzaloros <zacharymansellcello@gmail.com>
2020-11-14 16:34:47 -0600

    Add count unique values

---
+++ udemy-algorithms/countUniqueValues.js
@@ -0,0 +1,25 @@
+/* Using multiple pointers
+I: sorted array of numbers
+O:  number of unique values in the array
+C: TC / SC: O(n), bonus SC: O(n)
+E: countUniqueValues([111111,2]) 2
+Returns 0 if empty array
+
+
+*/
+
+const countUniqueValues = (sortedArray, { length } = sortedArray, let [i, j] = [0, 1]) => {
+  if (length === 0) return 0;
+  while (j < length) {
+    if (sortedArray[i] !== sortedArray[j]) {
+      i += 1;
+      sortedArray[i] = sortedArray[j];
+    }
+    j += 1;
+  }
+  return i + 1;
+}
+
+// countUniqueValues([1, 1, 1, 1, 1, 2]);
+// countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]);
+countUniqueValues([]);
