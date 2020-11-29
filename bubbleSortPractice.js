c8f6a9d1cb32dbca83e50c43fc1e40fd08ce1ac5
manzaloros <zacharymansellcello@gmail.com>
2020-11-29 14:32:15 -0600

    Add comment to bubble sort naive solution

--- bubbleSortPractice.js
+++ bubbleSortPractice.js
@@ -1,25 +1,26 @@
 //bubble sort
+// Should eventually make a sorting algo practice folder
 
 function bubbleSort(array) {
 	let length = array.length;
-  for (let i = 0; i < length; i++) {
-  	//iterates over each element
-  	//only loops if j is less than i subtracted from the total length
-  	//basically won't loop if i is the last index (3)
-  	for (let j = 0; j< (length - i - 1); j++) {
-  		//checks the adjacent element if its smaller than the current
-  		if (array[j] > array[j+1]) {
-  			//hold first element temporarily
-  			let tmp = array[j];
-  			//set first element equal to second
-  			array[j] = array[j+1];
-  			//set second element equal to the temporary variablwe
-  			array[j+1] = tmp;
-  		}
-  	}
-  }
-  return array;
+	for (let i = 0; i < length; i++) {
+		//iterates over each element
+		//only loops if j is less than i subtracted from the total length
+		//basically won't loop if i is the last index (3)
+		for (let j = 0; j < (length - i - 1); j++) {
+			//checks the adjacent element if its smaller than the current
+			if (array[j] > array[j + 1]) {
+				//hold first element temporarily
+				let tmp = array[j];
+				//set first element equal to second
+				array[j] = array[j + 1];
+				//set second element equal to the temporary variablwe
+				array[j + 1] = tmp;
+			}
+		}
+	}
+	return array;
 }
 
-let example = [7,3,5,0,9,1];
+let example = [7, 3, 5, 0, 9, 1];
 console.log(bubbleSort(example));
