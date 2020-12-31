95a6e201964be8ede89a860c37a4e2b3670121d9
manzaloros <zacharymansellcello@gmail.com>
2020-12-30 23:16:39 -0600

    Checking push to github

--- express-node-practice1/index.js
+++ express-node-practice1/index.js
@@ -1,10 +1,17 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
+
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
+  // Checking comment
+  res.send('Hello world!');
+});
+
+app.get('/', (req, res) => {
   // Checking comments
-  res.send("Hello world!");
+  res.send('Hello world!');
 });
 
 app.listen(port, () => {
