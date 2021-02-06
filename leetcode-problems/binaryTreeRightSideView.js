62b7fe148cb286282376eec34a7a486b44ac7b7f
manzaloros <zacharymansellcello@gmail.com>
2021-02-06 13:37:54 -0600

    Add binary tree right side view

---
+++ leetcode-problems/binaryTreeRightSideView.js
@@ -0,0 +1,38 @@
+/*
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+Example:
+
+Input: [1,2,3,null,5,null,4]
+Output: [1, 3, 4]
+Explanation:
+
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+  */
+
+const rightSideView = (root) => {
+  const queue = [];
+  if (!root) return queue;
+  const rightView = [];
+
+  queue.push(root);
+
+  while (queue.length > 0) {
+    const size = queue.length;
+    let val = 0;
+
+    for (let i = 0; i < size; i += 1) {
+      const node = queue.shift();
+      val = node.val;
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    rightView.push(val);
+  }
+  return rightView;
+};
