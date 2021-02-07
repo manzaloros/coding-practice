c2005914431f39c3efc232c88f7a40d1b3f690e6
manzaloros <zacharymansellcello@gmail.com>
2021-02-07 09:24:15 -0600

    Add BFS comment and fix long comment wrap

--- leetcode-problems/binaryTreeRightSideView.js
+++ leetcode-problems/binaryTreeRightSideView.js
@@ -1,11 +1,10 @@
 /*
-Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+Given a binary tree, imagine yourself standing on the right side of it, return
+the values of the nodes you can see ordered from top to bottom.
 
 Example:
 
-Input: [1,2,3,null,5,null,4]
-Output: [1, 3, 4]
-Explanation:
+Input: [1,2,3,null,5,null,4] Output: [1, 3, 4] Explanation:
 
    1            <---
  /   \
@@ -14,6 +13,7 @@ Explanation:
   5     4       <---
   */
 
+// BFS Breadth-First Search of Binary Tree
 const rightSideView = (root) => {
   const queue = [];
   if (!root) return queue;
@@ -22,11 +22,16 @@ const rightSideView = (root) => {
   queue.push(root);
 
   while (queue.length > 0) {
+    // number of nodes at current level
     const size = queue.length;
     let val = 0;
 
+    // traverse all nodes at current level
     for (let i = 0; i < size; i += 1) {
       const node = queue.shift();
+
+      // when the loop ends, `val` will contain the last node's value at the
+      // current level
       val = node.val;
 
       if (node.left) queue.push(node.left);
