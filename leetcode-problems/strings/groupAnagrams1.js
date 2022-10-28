b9102cafff6fbd54bf1937160eac96c43973bbfe
Zachary Mansell <zacharymansellcello@gmail.com>
2022-10-28 11:21:07 -0500

    Create groupAnagrams1.js

---
+++ leetcode-problems/strings/groupAnagrams1.js
@@ -0,0 +1,13 @@
+// Time: O(strs.length * (maxWordLength * log maxWordLength))
+// if max word length is 100, O(strs.length).
+// Space: O(strs.length) for new map
+const groupAnagrams = function(strs) {
+    return  [...strs.reduce((map, word) => {
+      const sorted = word.split('').sort().join('');
+      
+      if (!map.has(sorted)) map.set(sorted, []);
+      map.get(sorted).push(word);
+          
+    return map;
+    }, new Map()).values()]
+};
